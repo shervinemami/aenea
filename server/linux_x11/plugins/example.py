e6942d1af4c3236c647c4ccc1340b262ff0f6507
Shervin Emami <shervin.emami@gmail.com>
2019-04-16 00:20:20 +1000

    Add security token to the plugin example.

--- server/linux_x11/plugins/example.py
+++ server/linux_x11/plugins/example.py
@@ -3,7 +3,7 @@ from yapsy.IPlugin import IPlugin
 enabled = True
 
 
-def greet_user(name='Incognito'):
+def greet_user(name='Incognito', security_token=''):
     '''RPC command to greet a user. See the _server_plugin_example grammar for
        how to use on the client side via voice.'''
     print 'Hello user %s!' % name
